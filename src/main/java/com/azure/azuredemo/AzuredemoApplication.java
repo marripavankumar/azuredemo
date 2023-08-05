97c97c1dcc18efda12e9b5cab68b71d84bba84b3
pavan <marripavankumar@gmail.com>
2023-08-06 02:03:24 +0530

    "sample azure deploy"

---
+++ src/main/java/com/azure/azuredemo/AzuredemoApplication.java
@@ -0,0 +1,13 @@
+package com.azure.azuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuredemoApplication.class, args);
+	}
+
+}
